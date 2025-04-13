ce48c51f5e0aef9fa5d81188a23e129ffc7ec919
Igor Gorpinich <igor.gorpinich@outlook.be>
2025-04-13 10:12:02 +0200

    deploy docs

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,3 +1,5 @@
+
+
 with source as (
 
     select * from {{ source('raw', 'raw_customers') }}
@@ -7,9 +9,9 @@ with source as (
 renamed as (
 
     select
-        id as customer_id,
-        first_name,
-        last_name
+        id       as customer_id,
+        first_name as first_name,
+        last_name as last_name
     from source
 
 )
--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,18 +1,12 @@
 with source as (
-
     select * from {{ source('raw', 'raw_payments') }}
-
 ),
-
 renamed as (
-
     select
         id as payment_id,
         order_id,
         payment_method,
         amount
     from source
-
 )
-
 select * from renamed
