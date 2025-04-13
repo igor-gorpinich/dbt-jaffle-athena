28946a6c216e829aab680128041bd3981d976428
igor-gorpinich <igor-gorpinich@users.noreply.github.com>
2025-04-13 14:59:26 +0000

    deploy: 609c810d35209c469b7cabc598e394384f0004cf

---
+++ run/jaffle_athena/models/staging/stg_customers.sql
@@ -0,0 +1,20 @@
+create or replace view
+    "AwsDataCatalog"."jaffle_athena"."stg_customers"
+  as
+    with source as (
+
+    select * from AwsDataCatalog.jaffle_athena.raw_customers
+
+),
+
+renamed as (
+
+    select
+        id       as customer_id,
+        first_name as first_name,
+        last_name as last_name
+    from source
+
+)
+
+select * from renamed
